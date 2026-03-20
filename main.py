2be5b467f64f03a2eba4502a89c42323c820e897
dahyuhnah <dayanamarsya@gmail.com>
2026-03-20 11:24:59 +0800

    made some changes to the main codes

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from env import *
+from val_iter import *
+from pol_iter import *
+from plot import *
+
 def print_utilities(U):
     for r in range(rows):
         row_vals = []
@@ -7,3 +12,15 @@ def print_utilities(U):
             else:
                 row_vals.append(f'{U[(r, c)]:6.2f}')
         print(" ".join(row_vals))
+
+U_vi, vi_history = value_iteration(states)
+policy_vi = extract_policy(U_vi)
+
+print("~"*40)
+print("VALUE ITERATION UTILITIES")
+print_utilities(U_vi)
+print("\nVALUE ITERATION POLICY")
+print_utilities(policy_vi)
+
+plot_history(vi_history, [(0,0), (0,1), (1,2)], "VI Utility Estimates")
+plot_history(pi_history, [(0,0), (0,1), (1,2)], "PI Utility Estimates")
